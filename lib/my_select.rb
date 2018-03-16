50fb4a7c2095978659a188c37c5c956b52b2e00d
Daniel Brazel <dbrazel4@gmail.com>
2018-03-16 01:26:34 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,8 @@ def my_select(collection)
  i = 0
  true_arr = []
  while i < collection.length
-   true_arr << yield collection[i]
+   true_arr << (yield collection[i])
+   i += 1
  end
  true_arr
 end
